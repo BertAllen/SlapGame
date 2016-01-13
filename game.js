24786ff8b6182dfccd3f797fc8c9e2e07a7f3a18
Bert <Bert>
2016-01-12 20:43:00 -0700

    nearly done with part one

--- game.js
+++ game.js
@@ -1,12 +1,45 @@
 var ninjaHealth = 100;
 var playerHealth =100;
+var hits = 0;
 
-function slap(){
-    ninjaHealth--;
-
-//alert("ninjaHealth is now: " + ninjaHealth);
-}
+//keeps the player informed of their health
 function update(){
     document.getElementById("pHealth").innerText="Player Health is: "+playerHealth ;
+}
 
+//keeps the player informed of Ninja's health
+function nupdate(){
+    document.getElementById("nHealth").innerText="Ninja Health is: "+ninjaHealth ;
+}
+
+//tracks player hits
+function pHits(){
+    document.getElementById("playerHits").innerText="Total Player Hits: " + hits ;
+}
+
+//slaps ninja, reducing his health
+function slap(){
+    ninjaHealth--;
+    //this is to keep the health bars current
+update();
+nupdate();
+hits++
+}
+
+//punches ninja, reducing his health
+function punch(){
+    ninjaHealth -=5;
+    //this is to keep the health bars current
+update();
+nupdate();
+hits++
+}
+
+//kicks ninja, reducing his health
+function kick(){
+    ninjaHealth -=10;
+    //this is to keep the health bars current
+update();
+nupdate();
+hits++
 }
