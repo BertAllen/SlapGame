89eb55f665120a6087062d61ac937feaa9e16813
Bert <Bert>
2016-01-13 11:49:35 -0700

    merge conflict one

--- game.js
+++ game.js
@@ -1,51 +1,53 @@
-var ninjaHealth = 100;
+<<<<<<< HEAD
+
+=======
+>>>>>>> 9bccedfc1814db765e8dcb7d99774123e6af130d
 var playerHealth =100;
 var hits = 0;
 
 //keeps the player informed of their health
 function update(){
     document.getElementById("pHealth").innerText="Player Health is: "+playerHealth ;
-}
+<<<<<<< HEAD
+    
 
-//keeps the player informed of Ninja's health
-function nupdate(){
-    document.getElementById("nHealth").innerText="Ninja Health is: "+ninjaHealth ;
 
- if(ninjaHealth <= 0){
-         document.getElementById("player-panel").classList.add("panel-danger")
-    }else{
-         document.getElementById("player-panel").classList.remove("panel-danger")
-    }
+=======
 }
 
 //tracks player hits
 function pHits(){
+>>>>>>> 9bccedfc1814db765e8dcb7d99774123e6af130d
     document.getElementById("playerHits").innerText="Total Player Hits: " + hits ;
-}
+ }
 
 //slaps ninja, reducing his health
 function slap(){
-    ninjaHealth--;
+    playerHealth--;
     //this is to keep the health bars current
 update();
-nupdate();
+//nupdate();
 hits++; pHits()
 }
 
 //punches ninja, reducing his health
 function punch(){
-    ninjaHealth -=5;
+    playerHealth -=5;
     //this is to keep the health bars current
 update();
-nupdate();
+<<<<<<< HEAD
+//nupdate();
+=======
+nupdate()
+>>>>>>> 9bccedfc1814db765e8dcb7d99774123e6af130d
 hits++; pHits()
 }
 
 //kicks ninja, reducing his health
 function kick(){
-    ninjaHealth -=10;
+    playerHealth -=10;
     //this is to keep the health bars current
 update();
-nupdate();
+//nupdate();
 hits++; pHits()
 }
