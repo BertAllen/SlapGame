708d1493e642855c1bb35697aced500317b2ead2
Bert <Bert>
2016-01-13 15:31:47 -0700

    it's all good

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
-var playerHealth =100;
+//var playerHealth =100;
 var hits = 0;
 
 //keeps the player informed of their health
 function update(){
-    document.getElementById("pHealth").innerText="Player Health is: "+playerHealth ;
-document.getElementById("playerHits").innerText="Total Player Hits: " + hits ;
+    document.getElementById("pHealth").innerText="Player Health is: "+ Player.health ;
+document.getElementById("playerHits").innerText="Total Player Hits: " + Player.hits ;
 
 //this element should change the panel color 
-if(playerHealth <= 0){
+if(Player.health <= 0){
          document.getElementById("player-panel").classList.add("panel-danger");
     }else{
          document.getElementById("player-panel").classList.remove("panel-danger");
@@ -34,37 +34,37 @@ var items = {
 var Player = {
     health: 100,
     Pname: "",
-    hitcount: 0,
+    hits: 0,
     items: [items.shield],
 }
 
 //tracks player hits
-// function pHits(){
-//     }
+
+
 
 //slaps player, reducing his health
 function slap(){
-    playerHealth--;
+    Player.health--;
     //this is to keep the health bars current
-hits++; 
+Player.hits++; 
 update();
-//pHits()
+
 }
 
 //punches player, reducing his health
 function punch(){
-    playerHealth -=5;
+    Player.health -=5;
     //this is to keep the health bars current
-hits++; 
+Player.hits++; 
 update();
-//pHits()
+
 }
 
 //kicks player, reducing his health
 function kick(){
-    playerHealth -=10;
+    Player.health -=10;
     //this is to keep the health bars current
-hits++; 
+Player.hits++; 
 update();
-//pHits()
+
 }
