59b3ecd322a3bc426c153563a0a374a50b68b922
Bert <Bert>
2016-01-12 20:04:39 -0700

    working on health message

---
+++ game.js
@@ -0,0 +1,12 @@
+var ninjaHealth = 100;
+var playerHealth =100;
+
+function slap(){
+    ninjaHealth--;
+
+//alert("ninjaHealth is now: " + ninjaHealth);
+}
+function update(){
+    document.getElementById("pHealth").innerText="Player Health is: "+playerHealth ;
+
+}
